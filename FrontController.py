fe9038faa1f15b522fcaca9cfa9860d0508630f9
Galileo Guzmán <guzgali@gmail.com>
2015-05-25 11:01:29 -0500

    jugando con Sys.ARGV
    Descripción del uso de argumentos pasados por consola

--- FrontController.py
+++ FrontController.py
@@ -2,3 +2,13 @@
 import os
 import sys
 import BaseHTTPServer
+
+NOMBRE_SERVIDOR = 'localhost'
+PUERTO = 8000
+
+comandos = ['iniciar', 'parar', 'ayuda']
+
+if len(sys.argv) > 1:
+	print "Tienes argumentos"
+else:
+	print "no hay argumentos"
